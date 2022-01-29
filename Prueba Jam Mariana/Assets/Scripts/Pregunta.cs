d29db3251ab03f0f794ca57181481e5dc95fb1f9
milksake <caceresmariana443@gmail.com>
2022-01-29 18:29:48 -0500

    Mecanicas principales terminadas

--- Prueba Jam Mariana/Assets/Scripts/Pregunta.cs
+++ Prueba Jam Mariana/Assets/Scripts/Pregunta.cs
@@ -7,17 +7,18 @@ public class Pregunta
 {
     //Pregunta
     public string textoPregunta;
-    //Respuestas
+    //Respuestas y Valor
     public string textoRespuesta1;
+    public int valor1 = 0;
     public string textoRespuesta2;
-    //Valor
-    public int correcto = 0;
+    public int valor2 = 0;
 
-    public Pregunta(string textopreg, string texto1r, string texto2r, int correcto)
+    public Pregunta(string textopreg, string texto1r, int v1, string texto2r, int v2)
     {
         this.textoPregunta = textopreg;
         this.textoRespuesta1 = texto1r;
         this.textoRespuesta2 = texto2r;
-        this.correcto = correcto;
+        this.valor1 = v1;
+        this.valor2 = v2;
     }
 }
